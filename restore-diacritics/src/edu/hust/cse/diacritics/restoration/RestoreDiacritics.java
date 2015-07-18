ef2d2d5a80794f9bf7224b8561171f776393502b
Nhan Nguyen <nhannq88@gmail.com>
2015-07-18 11:07:23 -0400

    Change restore-diacritics

--- restore-diacritics/src/edu/hust/cse/diacritics/restoration/RestoreDiacritics.java
+++ restore-diacritics/src/edu/hust/cse/diacritics/restoration/RestoreDiacritics.java
@@ -7,136 +7,129 @@ import java.util.Hashtable;
 import java.util.StringTokenizer;
 
 public class RestoreDiacritics {
-	private static String MAP_FILE = "data/map.txt";
-	private static String BIGRAM_FILE = "data/bigram.txt";
-	private static int PLEN = 150;
-	private static int NB_ELEMENTS = 1000;
-	static Hashtable<String, String[]> htMap;
-	static Hashtable<String, Double> htProbability;
+  private static String MAP_FILE = "data/map.txt";
+  private static String BIGRAM_FILE = "data/bigram.txt";
+  private static int PLEN = 150;
+  private static int NB_ELEMENTS = 1000;
+  static Hashtable<String, String[]> htMap;
+  static Hashtable<String, Double> htProbability;
 
-	public static void init() {
-		htMap = new Hashtable<String, String[]>();
-		htProbability = new Hashtable<String, Double>();
-		loadDataBase();
-	}
+  public static void init() {
+    htMap = new Hashtable<String, String[]>();
+    htProbability = new Hashtable<String, Double>();
+    loadDataBase();
+  }
 
-	public static int loadDataBase() {
-		String s = new String();
-		try {
-			BufferedReader br = new BufferedReader(new InputStreamReader(
-					new FileInputStream(MAP_FILE), "UTF-16"));
-			while ((s = br.readLine()) != null) {
-				String temp[] = s.split("\\|");
-				if (temp.length == 2)
-					htMap.put(temp[0], temp[1].split(","));
-			}
-			br.close();
-			br = new BufferedReader(new InputStreamReader(new FileInputStream(
-					BIGRAM_FILE), "UTF-16"));
-			while ((s = br.readLine()) != null) {
-				String temp[] = s.split("\\|");
-				if (temp.length == 2)
-					htProbability.put(temp[0],
-							Double.valueOf(Double.parseDouble(temp[1])));
-			}
-			br.close();
-		} catch (Exception ex) {
-			System.out.print((new StringBuilder("Cannot loadDataBase: "))
-					.append(ex).toString());
-		}
-		return 1;
-	}
+  public static int loadDataBase() {
+    String s = new String();
+    try {
+      BufferedReader br =
+          new BufferedReader(new InputStreamReader(new FileInputStream(MAP_FILE), "UTF-16"));
+      while ((s = br.readLine()) != null) {
+        String temp[] = s.split("\\|");
+        if (temp.length == 2)
+          htMap.put(temp[0], temp[1].split(","));
+      }
+      br.close();
+      br = new BufferedReader(new InputStreamReader(new FileInputStream(BIGRAM_FILE), "UTF-16"));
+      while ((s = br.readLine()) != null) {
+        String temp[] = s.split("\\|");
+        if (temp.length == 2)
+          htProbability.put(temp[0], Double.valueOf(Double.parseDouble(temp[1])));
+      }
+      br.close();
+    } catch (Exception ex) {
+      System.out.print((new StringBuilder("Cannot loadDataBase: ")).append(ex).toString());
+    }
+    return 1;
+  }
 
-	public static String restore(String str) {
-		String s = new String();
-		double pNotHas = htProbability.get("notHas").doubleValue();
-		double p1[] = new double[PLEN];
-		double p2[] = new double[PLEN];
-		int track[][] = new int[NB_ELEMENTS][PLEN];
-		str = str.trim();
-		String word[] = str.split("[ ]+");
-		String wordResult[] = str.toLowerCase().split("[ ]+");
-		if (!htMap.containsKey(wordResult[0]))
-			htMap.put(wordResult[0], wordResult[0].split("\\s"));
-		String temp1[] = htMap.get(wordResult[0]);
-		for (int i = 1; i < wordResult.length; i++) {
-			if (!htMap.containsKey(wordResult[i]))
-				htMap.put(wordResult[i], wordResult[i].split("\\s"));
-			String temp2[] = (String[]) htMap.get(wordResult[i]);
-			for (int j = 0; j < temp2.length; j++) {
-				double ptemp[] = new double[150];
-				for (int k = 0; k < temp1.length; k++) {
-					s = new String(
-							(new StringBuilder(String.valueOf(temp1[k])))
-									.append(" ").append(temp2[j]).toString());
-					if (htProbability.containsKey(s))
-						ptemp[k] = p1[k] + htProbability.get(s).doubleValue();
-					else
-						ptemp[k] = p1[k] + pNotHas;
-				}
-				p2[j] = Utils.min(ptemp);
-				int pos = Utils.getPosInArr(p2[j], ptemp);
-				track[i][j] = i * 10 + pos;
-			}
-			Utils.moveArr(p1, p2);
-			temp1 = temp2;
-		}
-		int pos = Utils.getPosInArr(Utils.min(p1), p1);
-		for (int i = wordResult.length - 1; i >= 0; i--) {
-			temp1 = (String[]) htMap.get(wordResult[i]);
-			wordResult[i] = temp1[pos];
-			pos = track[i][pos] % 10;
-		}
-		s = new String();
-		for (int i = 0; i < wordResult.length; i++) {
-			String singleWord = wordResult[i];
-			if (!Utils.removeDiacritics(singleWord).equals(word[i])) {
-				char charArrWord[] = singleWord.toCharArray();
-				singleWord = new String();
-				for (int j = 0; j < charArrWord.length; j++) {
-					String ch = new String(charArrWord, j, 1);
-					if (word[i].charAt(j) > '@' && word[i].charAt(j) < '[')
-						ch = ch.toUpperCase();
-					singleWord = (new StringBuilder(String.valueOf(singleWord)))
-							.append(ch).toString();
-				}
+  public static String restore(String str) {
+    String s = new String();
+    double pNotHas = htProbability.get("notHas").doubleValue();
+    double p1[] = new double[PLEN];
+    double p2[] = new double[PLEN];
+    int track[][] = new int[NB_ELEMENTS][PLEN];
+    str = str.trim();
+    String word[] = str.split("[ ]+");
+    String wordResult[] = str.toLowerCase().split("[ ]+");
+    if (!htMap.containsKey(wordResult[0]))
+      htMap.put(wordResult[0], wordResult[0].split("\\s"));
+    String temp1[] = htMap.get(wordResult[0]);
+    for (int i = 1; i < wordResult.length; i++) {
+      if (!htMap.containsKey(wordResult[i]))
+        htMap.put(wordResult[i], wordResult[i].split("\\s"));
+      String temp2[] = (String[]) htMap.get(wordResult[i]);
+      for (int j = 0; j < temp2.length; j++) {
+        double ptemp[] = new double[150];
+        for (int k = 0; k < temp1.length; k++) {
+          s =
+              new String((new StringBuilder(String.valueOf(temp1[k]))).append(" ").append(temp2[j])
+                  .toString());
+          if (htProbability.containsKey(s))
+            ptemp[k] = p1[k] + htProbability.get(s).doubleValue();
+          else
+            ptemp[k] = p1[k] + pNotHas;
+        }
+        p2[j] = Utils.min(ptemp);
+        int pos = Utils.getPosInArr(p2[j], ptemp);
+        track[i][j] = i * 10 + pos;
+      }
+      Utils.moveArr(p1, p2);
+      temp1 = temp2;
+    }
+    int pos = Utils.getPosInArr(Utils.min(p1), p1);
+    for (int i = wordResult.length - 1; i >= 0; i--) {
+      temp1 = (String[]) htMap.get(wordResult[i]);
+      wordResult[i] = temp1[pos];
+      pos = track[i][pos] % 10;
+    }
+    s = new String();
+    for (int i = 0; i < wordResult.length; i++) {
+      String singleWord = wordResult[i];
+      if (!Utils.removeDiacritics(singleWord).equals(word[i])) {
+        char charArrWord[] = singleWord.toCharArray();
+        singleWord = new String();
+        for (int j = 0; j < charArrWord.length; j++) {
+          String ch = new String(charArrWord, j, 1);
+          if (word[i].charAt(j) > '@' && word[i].charAt(j) < '[')
+            ch = ch.toUpperCase();
+          singleWord = (new StringBuilder(String.valueOf(singleWord))).append(ch).toString();
+        }
 
-				wordResult[i] = singleWord;
-			}
-			s = (new StringBuilder(String.valueOf(s))).append(wordResult[i])
-					.append(" ").toString();
-		}
-		return s.trim();
-	}
+        wordResult[i] = singleWord;
+      }
+      s = (new StringBuilder(String.valueOf(s))).append(wordResult[i]).append(" ").toString();
+    }
+    return s.trim();
+  }
 
-	public static String restoreTextSegment(String textSegment) {
-		StringTokenizer strTokenizer = new StringTokenizer(textSegment,
-				"()[]/-\"\u2026',\u201C\u201D\u2018\u2019?:;", true);
-		String tempStr;
-		for (textSegment = new String(); strTokenizer.hasMoreTokens(); textSegment = (new StringBuilder(
-				String.valueOf(textSegment))).append(tempStr).toString()) {
-			boolean check = false;
-			tempStr = strTokenizer.nextToken();
-			if (tempStr.equals(".") || tempStr.equals(",")
-					|| tempStr.equals("?") || tempStr.equals("!")
-					|| tempStr.equals(":") || tempStr.equals(")")
-					|| tempStr.equals("]") || tempStr.equals("\u201D")
-					|| tempStr.equals("\u2019") || tempStr.equals("\u2026")) {
-				check = true;
-			}
-			if (tempStr.equals("(") || tempStr.equals("[")
-					|| tempStr.equals("\u201C") || tempStr.equals("\u2018")) {
-				check = true;
-			}
-			if (tempStr.equals("-")) {
-				check = true;
-			}
-			if (tempStr.equals("\"") || tempStr.equals("'")
-					|| tempStr.equals("\n") || tempStr.equals("/"))
-				check = true;
-			if (!check)
-				tempStr = " " + restore(tempStr) + " ";
-		}
-		return textSegment;
-	}
+  public static String restoreTextSegment(String textSegment) {
+    StringTokenizer strTokenizer =
+        new StringTokenizer(textSegment, "()[]/-\"\u2026',\u201C\u201D\u2018\u2019?:;", true);
+    String tempStr;
+    for (textSegment = new String(); strTokenizer.hasMoreTokens(); textSegment =
+        (new StringBuilder(String.valueOf(textSegment))).append(tempStr).toString()) {
+      boolean check = false;
+      tempStr = strTokenizer.nextToken();
+      if (tempStr.equals(".") || tempStr.equals(",") || tempStr.equals("?") || tempStr.equals("!")
+          || tempStr.equals(":") || tempStr.equals(")") || tempStr.equals("]")
+          || tempStr.equals("\u201D") || tempStr.equals("\u2019") || tempStr.equals("\u2026")) {
+        check = true;
+      }
+      if (tempStr.equals("(") || tempStr.equals("[") || tempStr.equals("\u201C")
+          || tempStr.equals("\u2018")) {
+        check = true;
+      }
+      if (tempStr.equals("-")) {
+        check = true;
+      }
+      if (tempStr.equals("\"") || tempStr.equals("'") || tempStr.equals("\n")
+          || tempStr.equals("/"))
+        check = true;
+      if (!check)
+        tempStr = " " + restore(tempStr) + " ";
+    }
+    return textSegment;
+  }
 }
--- restore-diacritics/src/edu/hust/cse/diacritics/restoration/Utils.java
+++ restore-diacritics/src/edu/hust/cse/diacritics/restoration/Utils.java
@@ -1,103 +1,103 @@
 package edu.hust.cse.diacritics.restoration;
 
 public class Utils {
-	public static String removeDiacritics(String s) {
-		s = s.replaceAll("\341", "a");
-		s = s.replaceAll("\340", "a");
-		s = s.replaceAll("\u1EA3", "a");
-		s = s.replaceAll("\u00E3", "a");
-		s = s.replaceAll("\u1EA1", "a");
-		s = s.replaceAll("\342", "a");
-		s = s.replaceAll("\u1EA5", "a");
-		s = s.replaceAll("\u1EA7", "a");
-		s = s.replaceAll("\u1EA9", "a");
-		s = s.replaceAll("\u1EAB", "a");
-		s = s.replaceAll("\u1EAD", "a");
-		s = s.replaceAll("\u0103", "a");
-		s = s.replaceAll("\u1EAF", "a");
-		s = s.replaceAll("\u1EB1", "a");
-		s = s.replaceAll("\u1EB3", "a");
-		s = s.replaceAll("\u1EB5", "a");
-		s = s.replaceAll("\u1EB7", "a");
-		s = s.replaceAll("\351", "e");
-		s = s.replaceAll("\350", "e");
-		s = s.replaceAll("\u1EBB", "e");
-		s = s.replaceAll("\u1EBD", "e");
-		s = s.replaceAll("\u1EB9", "e");
-		s = s.replaceAll("\352", "e");
-		s = s.replaceAll("\u1EBF", "e");
-		s = s.replaceAll("\u1EC1", "e");
-		s = s.replaceAll("\u1EC3", "e");
-		s = s.replaceAll("\u1EC5", "e");
-		s = s.replaceAll("\u1EC7", "e");
-		s = s.replaceAll("\363", "o");
-		s = s.replaceAll("\362", "o");
-		s = s.replaceAll("\u1ECF", "o");
-		s = s.replaceAll("\365", "o");
-		s = s.replaceAll("\u1ECD", "o");
-		s = s.replaceAll("\364", "o");
-		s = s.replaceAll("\u1ED3", "o");
-		s = s.replaceAll("\u1ED1", "o");
-		s = s.replaceAll("\u1ED5", "o");
-		s = s.replaceAll("\u1ED7", "o");
-		s = s.replaceAll("\u1ED9", "o");
-		s = s.replaceAll("\u01A1", "o");
-		s = s.replaceAll("\u1EDB", "o");
-		s = s.replaceAll("\u1EDD", "o");
-		s = s.replaceAll("\u1EDF", "o");
-		s = s.replaceAll("\u1EE1", "o");
-		s = s.replaceAll("\u1EE3", "o");
-		s = s.replaceAll("\355", "i");
-		s = s.replaceAll("\354", "i");
-		s = s.replaceAll("\u1EC9", "i");
-		s = s.replaceAll("\u0129", "i");
-		s = s.replaceAll("\u1ECB", "i");
-		s = s.replaceAll("\u00EC", "i");
-		s = s.replaceAll("\372", "u");
-		s = s.replaceAll("\371", "u");
-		s = s.replaceAll("\u1EE7", "u");
-		s = s.replaceAll("\u0169", "u");
-		s = s.replaceAll("\u01B0", "u");
-		s = s.replaceAll("\u1EE9", "u");
-		s = s.replaceAll("\u1EEB", "u");
-		s = s.replaceAll("\u1EED", "u");
-		s = s.replaceAll("\u00FA", "u");
-		s = s.replaceAll("\u1EEF", "u");
-		s = s.replaceAll("\u00F9", "u");
-		s = s.replaceAll("\u1EF1", "u");
-		s = s.replaceAll("\u0111", "d");
-		s = s.replaceAll("\375", "y");
-		s = s.replaceAll("\u1EF3", "y");
-		s = s.replaceAll("\u1EF7", "y");
-		s = s.replaceAll("\u1EF9", "y");
-		s = s.replaceAll("\u1EF5", "y");
-		return s;
-	}
+  public static String removeDiacritics(String s) {
+    s = s.replaceAll("\341", "a");
+    s = s.replaceAll("\340", "a");
+    s = s.replaceAll("\u1EA3", "a");
+    s = s.replaceAll("\u00E3", "a");
+    s = s.replaceAll("\u1EA1", "a");
+    s = s.replaceAll("\342", "a");
+    s = s.replaceAll("\u1EA5", "a");
+    s = s.replaceAll("\u1EA7", "a");
+    s = s.replaceAll("\u1EA9", "a");
+    s = s.replaceAll("\u1EAB", "a");
+    s = s.replaceAll("\u1EAD", "a");
+    s = s.replaceAll("\u0103", "a");
+    s = s.replaceAll("\u1EAF", "a");
+    s = s.replaceAll("\u1EB1", "a");
+    s = s.replaceAll("\u1EB3", "a");
+    s = s.replaceAll("\u1EB5", "a");
+    s = s.replaceAll("\u1EB7", "a");
+    s = s.replaceAll("\351", "e");
+    s = s.replaceAll("\350", "e");
+    s = s.replaceAll("\u1EBB", "e");
+    s = s.replaceAll("\u1EBD", "e");
+    s = s.replaceAll("\u1EB9", "e");
+    s = s.replaceAll("\352", "e");
+    s = s.replaceAll("\u1EBF", "e");
+    s = s.replaceAll("\u1EC1", "e");
+    s = s.replaceAll("\u1EC3", "e");
+    s = s.replaceAll("\u1EC5", "e");
+    s = s.replaceAll("\u1EC7", "e");
+    s = s.replaceAll("\363", "o");
+    s = s.replaceAll("\362", "o");
+    s = s.replaceAll("\u1ECF", "o");
+    s = s.replaceAll("\365", "o");
+    s = s.replaceAll("\u1ECD", "o");
+    s = s.replaceAll("\364", "o");
+    s = s.replaceAll("\u1ED3", "o");
+    s = s.replaceAll("\u1ED1", "o");
+    s = s.replaceAll("\u1ED5", "o");
+    s = s.replaceAll("\u1ED7", "o");
+    s = s.replaceAll("\u1ED9", "o");
+    s = s.replaceAll("\u01A1", "o");
+    s = s.replaceAll("\u1EDB", "o");
+    s = s.replaceAll("\u1EDD", "o");
+    s = s.replaceAll("\u1EDF", "o");
+    s = s.replaceAll("\u1EE1", "o");
+    s = s.replaceAll("\u1EE3", "o");
+    s = s.replaceAll("\355", "i");
+    s = s.replaceAll("\354", "i");
+    s = s.replaceAll("\u1EC9", "i");
+    s = s.replaceAll("\u0129", "i");
+    s = s.replaceAll("\u1ECB", "i");
+    s = s.replaceAll("\u00EC", "i");
+    s = s.replaceAll("\372", "u");
+    s = s.replaceAll("\371", "u");
+    s = s.replaceAll("\u1EE7", "u");
+    s = s.replaceAll("\u0169", "u");
+    s = s.replaceAll("\u01B0", "u");
+    s = s.replaceAll("\u1EE9", "u");
+    s = s.replaceAll("\u1EEB", "u");
+    s = s.replaceAll("\u1EED", "u");
+    s = s.replaceAll("\u00FA", "u");
+    s = s.replaceAll("\u1EEF", "u");
+    s = s.replaceAll("\u00F9", "u");
+    s = s.replaceAll("\u1EF1", "u");
+    s = s.replaceAll("\u0111", "d");
+    s = s.replaceAll("\375", "y");
+    s = s.replaceAll("\u1EF3", "y");
+    s = s.replaceAll("\u1EF7", "y");
+    s = s.replaceAll("\u1EF9", "y");
+    s = s.replaceAll("\u1EF5", "y");
+    return s;
+  }
 
-	public static double min(double arr[]) {
-		double min = arr[0];
-		for (int i = 1; i < arr.length; i++) {
-			double so = arr[i];
-			if (so != 0.0D)
-				min = min > so ? so : min;
-		}
-		return min;
-	}
+  public static double min(double arr[]) {
+    double min = arr[0];
+    for (int i = 1; i < arr.length; i++) {
+      double so = arr[i];
+      if (so != 0.0D)
+        min = min > so ? so : min;
+    }
+    return min;
+  }
 
-	public static int getPosInArr(double so, double arr[]) {
-		for (int i = 0; i < arr.length; i++)
-			if (so == arr[i])
-				return i;
-		return -1;
-	}
+  public static int getPosInArr(double so, double arr[]) {
+    for (int i = 0; i < arr.length; i++)
+      if (so == arr[i])
+        return i;
+    return -1;
+  }
 
-	public static boolean moveArr(double arr1[], double arr2[]) {
-		if (arr1.length < arr2.length)
-			return false;
-		for (int i = 0; i < arr2.length; i++) {
-			arr1[i] = arr2[i];
-			arr2[i] = 0.0D;
-		}
-		return true;
-	}
+  public static boolean moveArr(double arr1[], double arr2[]) {
+    if (arr1.length < arr2.length)
+      return false;
+    for (int i = 0; i < arr2.length; i++) {
+      arr1[i] = arr2[i];
+      arr2[i] = 0.0D;
+    }
+    return true;
+  }
 }
